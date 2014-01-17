466d0b138a7eaea893747a8671f027933da69fd3
Berkus <berkus@gmail.com>
2014-01-17 15:13:02 +0200

    start lectures

---
+++ json.scala
@@ -0,0 +1,25 @@
+abstract class JSON
+case class JSeq(elems: List[JSON]) extends JSON
+case class JObj(bindings: Map[String, JSON]) extends JSON
+case class JNum(num: Double) extends JSON
+case class JStr(str: String) extends JSON
+case class JBool(b: Boolean) extends JSON
+case object JNull extends JSON
+
+val data = JObj(Map(
+    "firstName" -> JStr("John"),
+    "lastName" -> JStr("Smith"),
+    "address" -> JObj(Map(
+        "streetAddress" -> JStr("21 2nd Street"),
+        "state" -> JStr("NY"),
+        "postalCode" -> JNum(10021)
+    )),
+    "phoneNumbers" -> JSeq(List(
+        JObj(Map(
+            "type" -> JStr("Home"), "number" -> JStr("212 555-1234")
+        )),
+        JObj(Map(
+            "type" -> JStr("work"), "number" -> JStr("656 555-4321")
+        ))
+    ))
+))
